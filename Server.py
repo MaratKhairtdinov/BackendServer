b880c9405e54d4f4bead82b15e78be7127bf7284
MaratKhairtdinov <marrou7@gmail.com>
2020-09-11 11:45:41 +0200

    SERVER WORKS PERFECTLY!!! Sends and receives data in separate threads

--- Server.py
+++ Server.py
@@ -84,7 +84,7 @@ class RegistrationManager():
         result = o3d.registration.registration_icp(
                 source, target, distance_threshold, np.identity(4),
                 o3d.registration.TransformationEstimationPointToPlane()).transformation
-        print(":: ICP result: ")
+        print(":: ICP result:")
         print(result)        
         return result
         
@@ -100,7 +100,7 @@ class RegistrationManager():
         voxel_size = 1        
         source_down, target_down, source_fpfh, target_fpfh = self.prepare_dataset(voxel_size, self.source, self.target)   
         
-        result_ransac = self.execute_global_registration(source_down, target_down, source_fpfh, target_fpfh, voxel_size)        
+        result_ransac = self.execute_global_registration(source_down, target_down, source_fpfh, target_fpfh, voxel_size)
         result_icp = self.refine_registration(self.source, self.target, voxel_size, result_ransac)
         
         self.result = result_icp.dot(result_ransac)
@@ -109,11 +109,10 @@ class RegistrationManager():
         source = copy.deepcopy(self.model_loader.pointcloud)
         source.paint_uniform_color([1,0,0])
         self.draw_registration_result(source, target, self.result)
-        
 
 class NetworkDataHandler:
-    def __init__(self, server):
-        self.server = server
+    def __init__(self, client):
+        self.client = client
         self.last_buffer_sent = None;
         self.last_data_type_sent = None;
         self.pointcloud = None
@@ -124,7 +123,7 @@ class NetworkDataHandler:
         if response == NetworkResponseType.AllGood.value:
             pass
         elif response == NetworkResponseType.DataCorrupt.value:
-            self.server.send(self.conn, self.addr, self.last_data_type_sent, self.last_buffer_sent)
+            self.client.send(self.last_data_type_sent, self.last_buffer_sent)
             
     def handle_string(self, buffer):
         response = None
@@ -133,7 +132,7 @@ class NetworkDataHandler:
             response = NetworkResponseType.AllGood
         except:
             response = NetworkResponseType.DataCorrupt            
-        self.server.send_response(self.conn, self.addr, response)
+        self.client.send_response(response)
 
     def write_PCD(self, pointcloud):
         o3d.io.write_point_cloud("ReceivedPointcloud.pcd", pointcloud, False, False, True)
@@ -158,18 +157,16 @@ class NetworkDataHandler:
         except Exception:
             response = NetworkResponseType.DataCorrupt
         
-        self.server.send_response(self.conn, self.addr, response)
+        self.client.send_response(response)
         self.pointcloud = o3d.geometry.PointCloud(points = o3d.utility.Vector3dVector(points))
         self.pointcloud.normals = o3d.utility.Vector3dVector(normals)
         
         self.write_PCD(self.pointcloud)
         self.registration_manager.execute_registration(self.pointcloud)
-        self.server.send_matrix(self.conn, self.addr, self.registration_manager.result)
+        self.client.send_matrix(self.registration_manager.result)
         
         
-    def handle_network_data(self, conn, addr, data_type, buffer):
-        self.conn = conn
-        self.addr = addr
+    def handle_network_data(self, data_type, buffer):
     
         if data_type == NetworkDataType.String.value:
             self.handle_string(buffer)
@@ -179,102 +176,118 @@ class NetworkDataHandler:
             self.handle_point_cloud(buffer)
         
 
-class Server:
-    def __init__(self, TCP_IP, INPUT_PORT, OUTPUT_PORT):
-        print("[STARTING] Server is starting")
-        self.TCP_IP = TCP_IP
+class InputGateThread(threading.Thread):
+    def __init__(self, client, ip, port):
+        self.client = client
+        self.IP = ip
+        self.PORT = port        
+        self.gate = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        
+        self.receiving = True
+        threading.Thread.__init__(self)
         
-        self.INPUT_PORT   = INPUT_PORT
-        self.OUTPUT_PORT  = OUTPUT_PORT
+        print("[ INPUT GATE INITIALIZED]")
         
-        self.input_gate = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.output_gate = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def run(self):                
+        self.gate.bind((self.IP, self.PORT))
+        self.gate.listen()
+        print("[ Input Gate is listening...]")
+        self.conn, self.address = self.gate.accept()
+        print("[ CLIENT CONNECTED TO INPUT GATE]")
+        self.receive()
         
-        self.input_gate.bind((self.TCP_IP, self.INPUT_PORT))        
-        self.output_gate.bind((self.TCP_IP, self.OUTPUT_PORT))
-        self.data_handler = NetworkDataHandler(self)
-        
-        self.handlers = []
-        self.receiving = False
-        
-    def send(self, conn, addr, data_type, buffer):
-        conn.send(struct.pack('>h',data_type.value))
-        length_buffer = struct.pack('i',len(buffer))
-        conn.send(length_buffer)
-        conn.send(buffer)
-        print("data sent")
-
-    def send_matrix(self, conn, addr, matrix):
-        buffer = []
-        for column in range(matrix.shape[1]):
-            for row in range(matrix.shape[0]):
-                buffer.append(struct.pack('d',matrix[row, column]))
-        buffer = b''.join(buffer)
-        self.send(conn, addr, NetworkDataType.Matrix, buffer)
-        print("Matrix sent")
-        
-    def send_response(self, conn, addr, response):        
-        self.send(conn, addr, NetworkDataType.Response, struct.pack('>h', response.value))
-        print(f"{response.name} sent back")
-        
-    def listen(self, conn_input, addr_input, conn_output, addr_output ):
-        receiving = True        
-        while receiving:
+    def receive(self):        
+        while self.receiving:
             input_buff = []
-            data_type  = struct.unpack('>h', conn_input.recv(2))[0]
-            chunk_size = struct.unpack('>i', conn_input.recv(4))[0]
-            chunks     = struct.unpack('>i', conn_input.recv(4))[0]
-            residual   = struct.unpack('>i', conn_input.recv(4))[0]
+            data_type  = struct.unpack('>h', self.conn.recv(2))[0]
+            chunk_size = struct.unpack('>i', self.conn.recv(4))[0]
+            chunks     = struct.unpack('>i', self.conn.recv(4))[0]
+            residual   = struct.unpack('>i', self.conn.recv(4))[0]
             print(f"{chunks} chunks of size {chunk_size} expected")
             errorLog = ""
             for i in range(chunks):
                 received = False
                 while not received:
-                    chunk = conn_input.recv(chunk_size)
+                    chunk = self.conn.recv(chunk_size)
                     if len(chunk)==chunk_size:
                         input_buff.append(chunk)
-                        conn_input.send(struct.pack('>h', NetworkResponseType.AllGood.value))
+                        self.conn.send(struct.pack('>h', NetworkResponseType.AllGood.value))
                         received = True
                         print(f"Chunk #{i} received")
                     else:
                         errorLog+="\nChunk #{i} corrupt"
-                        conn_input.send_input(struct.pack('>h', NetworkResponseType.DataCorrupt.value))                
-            rest_data = conn_input.recv(residual)
+                        self.conn.send(struct.pack('>h', NetworkResponseType.DataCorrupt.value))
+            rest_data = self.conn.recv(residual)
             last_chunk_received = False
             while not last_chunk_received:
                 if len(rest_data)== residual:
                     input_buff.append(rest_data)
-                    conn_input.send(struct.pack('>h', NetworkResponseType.AllGood.value))
+                    self.conn.send(struct.pack('>h', NetworkResponseType.AllGood.value))
                     last_chunk_received = True
                 else:
                     errorLog+="\nLast chunk corrupt"
-                    conn_input.send(struct.pack('>h', NetworkResponseType.DataCorrupt.value))                
-            
+                    self.conn.send(struct.pack('>h', NetworkResponseType.DataCorrupt.value))            
             input_buff = b''.join(input_buff)
-
-            print(f"Data received, type: {data_type}")
-            print(f"ErrorLog:{errorLog}")
-            self.data_handler.handle_network_data(conn_output, addr_output, data_type, input_buff)
+            self.client.network_data_handler.handle_network_data(data_type, input_buff)
+            
+            
+class OutputGateThread(threading.Thread):
+    def __init__(self, client, ip, port):
+        self.client = client
+        self.IP = ip
+        self.PORT = port
+        self.gate = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.receiving = True
+        threading.Thread.__init__(self)
+        print("[OUTPUT GATE INITIALIZED]")
         
-    def start_listening(self, number_of_clients):
-        self.input_gate.listen()
-        self.output_gate.listen()
-        print("[LISTENING] Server is listening on{SERVER}")
-        for i in range(number_of_clients):
-            print(f"[ACTIVE CONNECTIONS]{threading.activeCount()-1}")
-            server = self
-            conn_input, addr_input = self.input_gate.accept()            
-            time.sleep(1)
-            conn_output, addr_output = self.input_gate.accept()
-            #self.handlers.append(NetworkDataHandler(server, conn, addr))
-            thread = threading.Thread(target = self.listen, args = (conn_input, addr_input, conn_output, addr_output))
-            thread.start()
-
-TCP_IP = "192.168.0.100"
-INPUT_PORT = 6000
+    def run(self):
+        self.gate.bind((self.IP, self.PORT))
+        self.gate.listen()
+        print("[Output Gate is listening...]")
+        self.conn, self.address = self.gate.accept()
+        print("[CLIENT CONNECTED TO OUTPUT GATE]")
+        
+    def send(self, data_type, buffer):
+        self.conn.send(struct.pack('h',data_type.value))
+        length_buffer = struct.pack('i',len(buffer))
+        self.conn.send(length_buffer)
+        self.conn.send(buffer)
+        print(f"data sent {data_type.value}, buffer length: {len(buffer)}")
+        
+class Client:
+    def __init__(self, HOST, INPUT_PORT, OUTPUT_PORT):
+        print("[STARTING SERVER]")
+        self.input_gate = InputGateThread(self, HOST, INPUT_PORT)
+        self.output_gate = OutputGateThread(self, HOST ,OUTPUT_PORT)
+        self.network_data_handler = NetworkDataHandler(self)
+    def start(self):        
+        self.input_gate.start()
+        self.output_gate.start()        
+    def send_matrix(self, matrix):
+        buffer = []
+        for column in range(matrix.shape[1]):
+            for row in range(matrix.shape[0]):
+                buffer.append(struct.pack('d',matrix[row, column]))        
+        buffer = b''.join(buffer)
+        self.output_gate.send(NetworkDataType.Matrix, buffer)
+    def send_response(self, response):
+        self.output_gate.send(NetworkDataType.Response, struct.pack('>h', response.value))
+        print(f"{response.name} sent back")
+        
+class Server:
+    def __init__(self, HOST, INPUT_PORT, OUTPUT_PORT):
+        self.client = Client(HOST, INPUT_PORT, OUTPUT_PORT)
+    def start_clients(self):
+        self.client.start()
+        
+        
+        
+HOST = "192.168.0.100"
+INPUT_PORT  = 6000
 OUTPUT_PORT = 5000
 
-server = Server(TCP_IP, INPUT_PORT, OUTPUT_PORT)
-server.start_listening(5)
+server = Server(HOST, INPUT_PORT, OUTPUT_PORT)
+server.start_clients()
 
-input()
+          
+            
